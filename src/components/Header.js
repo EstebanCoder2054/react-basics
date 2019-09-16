d264f63c423bf4cc9d49a57e84ff97c954937da8
Esteban <esteban-escobar1111@hotmail.com>
2019-09-15 23:05:25 -0500

    v0.0.1

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+    render(){
+        return(
+            <div>
+                <Header titulo='Tienda Virtual' 
+                
+                />
+            </div>
+        );
+    }
+}
+
+export default Header;
